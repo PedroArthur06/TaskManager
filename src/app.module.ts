929022758679fd88ad1a7a11fdd12a0ab8c5b936
Pedro Arthur <pedroxxl917@gmail.com>
2026-02-21 18:12:36 -0400

    feat: Implement task module with basic CRUD operations, including service, controller, DTOs, entity, and tests.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [TaskModule],
