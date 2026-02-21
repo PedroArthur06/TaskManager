48090d5046865084603ace21188e589d20e6415c
Pedro Arthur <pedroxxl917@gmail.com>
2026-02-21 18:44:12 -0400

    feat: Add validation rules to CreateTaskDto and install class-validator dependencies.

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,1 +1,23 @@
-export class CreateTaskDto {}
+import { IsDate, IsIn, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class CreateTaskDto {
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(3)
+    title: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(10)
+    description: string;
+
+    @IsNotEmpty()
+    @IsDate()
+    dueDate: Date;
+
+    @IsNotEmpty()
+    @IsString()
+    @IsIn(['pending', 'in-progress', 'completed'])
+    status: string;
+}
